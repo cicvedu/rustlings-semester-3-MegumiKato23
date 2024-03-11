537160a5ef1951cf51e9cefffbdd311e0d66d0f5
MegumiKato <531197875@qq.com>
2024-03-11 15:48:57 +0800

    update

--- exercises/enums/enums1.rs
+++ exercises/enums/enums1.rs
@@ -2,16 +2,20 @@
 //
 // No hints this time! ;)
 
-// I AM NOT DONE
+
 
 #[derive(Debug)]
 enum Message {
     // TODO: define a few types of messages as used below
+    Quit,
+    Echo(String),
+    Move { x: i32, y: i32 },
+    ChangeColor(i32, i32, i32),
 }
 
 fn main() {
     println!("{:?}", Message::Quit);
-    println!("{:?}", Message::Echo);
-    println!("{:?}", Message::Move);
-    println!("{:?}", Message::ChangeColor);
+    println!("{:?}", Message::Echo(String::from("zzzz")));
+    println!("{:?}", Message::Move { x: 5, y: 4 });
+    println!("{:?}", Message::ChangeColor(0, 0, 0));
 }
